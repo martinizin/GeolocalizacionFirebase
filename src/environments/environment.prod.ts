3880ebb6d35ccda81586519ae66a8349975e4485
martinizin <mar45pillacela@gmail.com>
2024-06-10 11:46:39 -0500

    Construcción de apk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAA3lvXerCdkEjdB8WUIbxW58FOevr1pBo",
+    authDomain: "geolocation-760d8.firebaseapp.com",
+    projectId: "geolocation-760d8",
+    storageBucket: "geolocation-760d8.appspot.com",
+    messagingSenderId: "580947949932",
+    appId: "1:580947949932:web:95b26e21f6d13a2634db49"
+  }
 };
